cec320cffc6347fbc8a6b8531502c8c72d74eacc
OllisGit <olaf.kaus+github@gmail.com>
2019-09-01 09:11:47 +0200

    start

--- octoprint_AutostartPrint/__init__.py
+++ octoprint_AutostartPrint/__init__.py
@@ -1,19 +1,23 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
+
 class AutostartPrintPlugin(octoprint.plugin.SettingsPlugin,
-                           octoprint.plugin.AssetPlugin,
-                           octoprint.plugin.TemplatePlugin):
+						   octoprint.plugin.AssetPlugin,
+						   octoprint.plugin.TemplatePlugin,
+						   octoprint.plugin.StartupPlugin,
+						   octoprint.plugin.EventHandlerPlugin):
+
+	def on_startup(self, host, port):
+		self._logger.info("ONSTART!")
+
+#	def on_after_startup(self):
+#		self._logger.info("AFTERSTART")
+
+	def on_event(self, event, payload):
+		self._logger.info("EVENT Hello World!")
 
 	##~~ SettingsPlugin mixin
 
@@ -61,6 +65,7 @@ class AutostartPrintPlugin(octoprint.plugin.SettingsPlugin,
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "AutostartPrint Plugin"
 
+
 def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = AutostartPrintPlugin()
@@ -69,4 +74,3 @@ def __plugin_load__():
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 	}
-
